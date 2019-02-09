33cbee81b5c441c748d62b8c6148be9b02f86395
Eeshani Patel <eeshani.p@keka.com>
2019-02-09 22:42:44 +0530

    access control allow orgin headers added to connet with api server

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,15 @@ var messages =  [
     }
 ];
 
-app.get('/messages', (req, res) => {
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get('/api/messages', (req, res) => {
     res.json(messages);      //send response
 });
 
-app.listen(4200);
+
+app.listen(1234);
